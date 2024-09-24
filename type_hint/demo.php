f6fcc3c212eec937116fa1259cb06663ec0b0d1e
khang03 <nguyenthekhang2210@gmail.com>
2024-09-24 16:04:22 +0700

    hello

---
+++ type_hint/demo.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+require_once('I.php');
+require_once('A.php');
+require_once('B.php');
+require_once('C.php');
+class Demo
+{
+    public function typeAReturnA(): A
+    {
+        echo __FUNCTION__ . "<br>";
+        return new A();
+    }
+    public function typeAReturnB(): A
+    {
+        echo __FUNCTION__ . "<br>";
+        return new B();
+    }
+    public function typeAReturnC(): A
+    {
+        echo __FUNCTION__ . "<br>";
+        return new C();
+    }
+    public function typeAReturnNull(): A
+    {
+        echo __FUNCTION__ . "<br>";
+        return new null;
+    }
+    public function typeAReturnI(): A
+    {
+        echo __FUNCTION__ . "<br>";
+        return new I();
+    }
+
+    //Check B-----------------------------------
+    public function typeBReturnA(): B
+    {
+        echo __FUNCTION__ . "<br>";
+        return new A();
+    }
+    public function typeBReturnB(): B
+    {
+        echo __FUNCTION__ . "<br>";
+        return new B();
+    }
+    public function typeBReturnC(): B
+    {
+        echo __FUNCTION__ . "<br>";
+        return new C();
+    }
+    public function typeBReturnNull(): B
+    {
+        echo __FUNCTION__ . "<br>";
+        return new null;
+    }
+    public function typeBReturnI(): B
+    {
+        echo __FUNCTION__ . "<br>";
+        return new I();
+    }
+    //Check C--------------------------------------
+    public function typeCReturnA(): C
+    {
+        echo __FUNCTION__ . "<br>";
+        return new A();
+    }
+    public function typeCReturnB(): C
+    {
+        echo __FUNCTION__ . "<br>";
+        return new B();
+    }
+    public function typeCReturnC(): C
+    {
+        echo __FUNCTION__ . "<br>";
+        return new C();
+    }
+    public function typeCReturnNull(): C
+    {
+        echo __FUNCTION__ . "<br>";
+        return new null;
+    }
+    public function typeCReturnI(): C
+    {
+        echo __FUNCTION__ . "<br>";
+        return new I();
+    }
+
+    //Check I------------------------------------------
+    public function typeIReturnA(): I
+    {
+        echo __FUNCTION__ . "<br>";
+        return new A();
+    }
+    public function typeIReturnB(): I
+    {
+        echo __FUNCTION__ . "<br>";
+        return new B();
+    }
+    public function typeIReturnC(): I
+    {
+        echo __FUNCTION__ . "<br>";
+        return new C();
+    }
+    public function typeIReturnNull(): I
+    {
+        echo __FUNCTION__ . "<br>";
+        return new null;
+    }
+    public function typeIReturnI(): I
+    {
+        echo __FUNCTION__ . "<br>";
+        return new I();
+    }
+    //Check NULL---------------------------------------
+    public function typeNullReturnA(): NULL
+    {
+        echo __FUNCTION__ . "<br>";
+        return new A();
+    }
+    public function typeNullReturnB(): NULL
+    {
+        echo __FUNCTION__ . "<br>";
+        return new B();
+    }
+    public function typeNullReturnC(): NULL
+    {
+        echo __FUNCTION__ . "<br>";
+        return new C();
+    }
+    public function typeNullReturnNull(): NULL
+    {
+        echo __FUNCTION__ . "<br>";
+        return new null;
+    }
+    public function typeNullReturnI(): NULL
+    {
+        echo __FUNCTION__ . "<br>";
+        return new I();
+    }
+}
+$demo = new Demo();
+$demo->typeNullReturnNull();
+$demo->typeAReturnA();
